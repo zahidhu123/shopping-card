fa5dcdcfb9d5f86d611cfed82ed9920ead62fb33
unknown <zahidmd1996@gmail.com>
2022-11-02 01:26:26 +0530

    minues

--- src/component/cartList.js
+++ src/component/cartList.js
@@ -1,24 +1,54 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
 
 export default function CartList({ cart }) {
+
+  const [CART, setCART] = useState([])
+
+  useEffect(() => {
+    setCART(cart)
+  }, [cart])
+
+
+
   return (
     <div className='container'>
       <div className="row">
-        <div className="col-lg-5">
+        <div className="col-lg-5 mt-4">
           {
-            cart.map((cartItem, cartIndex) => {
+            CART?.map((cartItem, cartIndex) => {
               return (
                 <div>
                   <img src={cartItem.url} width={40} alt="image" />
                   <span>{cartItem.name}</span>
-                  <button>-</button>
+                  <button
+                    onClick={() => {
+                      const _CART = CART.map((item, index) => {
+                        return cartIndex === index ? { ...item, quantity: item.quantity > 0 ? item.quantity - 1 : 0 } : item
+                      })
+                      setCART(_CART)
+                    }}
+                  >-</button>
                   <span>{cartItem.quantity}</span>
-                  <button>+</button>
-                  <span>Rs. {cartItem.price}</span>
+                  <button
+                    onClick={() => {
+                      const _CART = CART.map((item, index) => {
+                        return cartIndex === index ? { ...item, quantity: item.quantity + 1 } : item
+                      })
+                      setCART(_CART)
+                    }}
+                  >+</button>
+                  <span>Rs. {cartItem.price * cartItem.quantity}</span>
                 </div>
               )
             })
           }
+          <p> Total Rs. <span></span>
+            {
+              CART.map(item => item.price * item.quantity).reduce((total, value) => total + value, 0)
+            }
+          </p>
         </div>
       </div>
     </div>
